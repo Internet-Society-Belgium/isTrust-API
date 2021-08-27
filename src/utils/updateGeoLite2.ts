a17a6f0fed49922cccaa7f7c3bf9bb8ace6284ab
Progitiel <github@progiciel.be>
2021-08-27 18:05:03 +0200

    #1 update GeoLite2 db

---
+++ src/utils/updateGeoLite2.ts
@@ -0,0 +1,78 @@
+import fs from 'fs-extra'
+import tar from 'tar'
+import path from 'path'
+import https from 'https'
+import crypto from 'crypto'
+
+const filesPath = path.resolve(__dirname, '../bin/GeoLite2-Country')
+const dbPath = path.resolve(filesPath, 'GeoLite2-Country.mmdb')
+
+export default async () => {
+  if (fs.existsSync(dbPath)) {
+    const { ctime: lastUpdate } = fs.statSync(dbPath)
+
+    console.log(lastUpdate)
+
+    const now = new Date()
+    const outdated = new Date()
+    outdated.setDate(lastUpdate.getDate() + 25)
+
+    console.log(now)
+    console.log(outdated)
+
+    if (now < outdated) return dbPath
+  }
+
+  await update()
+
+  return dbPath
+}
+
+async function update() {
+  const licenseKey = process.env.MAXMIND_LICENCE_KEY
+
+  if (!licenseKey) throw new Error('No licence Key')
+
+  const archiveUrl = `https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=${licenseKey}&suffix=tar.gz`
+  const checksumUrl = `${archiveUrl}.sha256`
+
+  const archivePath = path.resolve(filesPath, 'GeoLite2-Country.tar.gz')
+  const checksumPath = path.resolve(filesPath, 'GeoLite2-Country.sha256')
+
+  fs.removeSync(filesPath)
+  fs.ensureDirSync(filesPath)
+
+  const p1 = download(checksumUrl, checksumPath)
+  const p2 = download(archiveUrl, archivePath)
+
+  await Promise.all([p1, p2])
+
+  const checksumFile = fs.readFileSync(checksumPath, { encoding: 'utf-8' })
+  const checksum = checksumFile.split(' ')[0]
+  const archiveChecksum = fileHash(archivePath)
+
+  if (checksum !== archiveChecksum) throw new Error('Invalid Hash')
+
+  tar.x({ file: archivePath, sync: true, strip: 1, cwd: filesPath })
+
+  fs.remove(checksumPath)
+  fs.remove(archivePath)
+}
+
+function download(url: string, path: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    https.get(url, (response) => {
+      if (response.statusCode !== 200) reject()
+      response.pipe(fs.createWriteStream(path)).on('finish', () => {
+        resolve()
+      })
+    })
+  })
+}
+
+function fileHash(filePath: string) {
+  return crypto
+    .createHash('sha256')
+    .update(fs.readFileSync(filePath))
+    .digest('hex')
+}
