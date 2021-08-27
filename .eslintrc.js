60a13f75d3807e0dfd63bd6b27c6f1a27d2c797f
Progitiel <github@progiciel.be>
2021-08-27 19:25:49 +0200

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,8 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
+    root: true,
+    env: {
+        node: true,
+    },
+    parser: '@typescript-eslint/parser',
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
 }
