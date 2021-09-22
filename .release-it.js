4e65f009512a1e20be74b32a42e3d4bc2ddecc51
Progitiel <github@progiciel.be>
2021-09-22 14:17:26 +0200

    refactor: migrate to serverless
    #21

--- .release-it.js
+++ .release-it.js
@@ -1,21 +1,12 @@
 module.exports = {
     hooks: {
         'before:init': ['npm run lint', 'npm run format'],
-        'after:bump': 'npm run build',
-        'after:git:release': 'npm run pack',
-        'after:release':
-            'echo Successfully released ${name} v${version} to ${repo.repository}.',
     },
     git: {
         commitMessage: 'chore: release v${version}',
     },
     npm: {
         publish: false,
-    },
-    github: {
-        release: true,
-        releaseName: '${version}',
-        assets: ['pack/*.zip'],
     },
     plugins: {
         '@release-it/conventional-changelog': {
