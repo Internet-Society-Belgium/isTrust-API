83a0f8cbf8ced304f92480b6736edefaad5abaf6
Progitiel <github@progiciel.be>
2021-09-22 16:04:28 +0200

    feat: /certificate

---
+++ src/types/certificate.ts
@@ -0,0 +1,19 @@
+export interface ValidCertificate {
+    valid: true
+    protocol: string
+    owner?: Owner
+}
+
+export interface Owner {
+    organisation: string
+    location?: {
+        state: string
+        region: string
+        country: string
+    }
+}
+
+export interface InvalidCertificate {
+    valid: false
+    error: string
+}
